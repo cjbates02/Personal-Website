96d859ea95fc360e8446ad2ce7c41a7ef727ac4e
cjbates02 <christianbates02@gmail.com>
2024-11-03 12:20:22 -0500

    frontend and backend implementation including navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <div className="navbar-container">
+            <div className="navbar-title-container">
+                <span>Christian Bates</span>
+            </div>
+            <div className="navbar-btns">
+                <span className="navbar-button">About me</span>
+                <span className="navbar-button">Blog</span>
+                <span className="navbar-button">Resume</span>
+            </div>
+        </div>
+    );
+};

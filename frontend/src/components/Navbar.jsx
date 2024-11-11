3c2e8f39823e2f507c6ac6a972c81e0f53c35860
cjbates02 <christianbates02@gmail.com>
2024-11-10 22:41:23 -0500

    added icons, added links for github and linked in

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,9 @@
+import IconEmail from "../assets/icons/email.svg"
+import IconPhone from "../assets/icons/phone.svg"
+import IconGitHub from "../assets/icons/github.svg"
+import IconLinkedIn from "../assets/icons/linkedin.svg"
+
+
 
 function Navbar({ setShowAboutMe, showAboutMe }) {
     function handleAboutMeClick() {
@@ -11,6 +17,12 @@ function Navbar({ setShowAboutMe, showAboutMe }) {
                 <a className="resume-upload-a" href="http://127.0.0.1:5000/download/resume">
                     <span className="navbar-button">Resume</span>
                 </a>
+            </div>
+            <div className="contact-me-container">
+                <a href="https://www.linkedin.com/in/christianbates02/"><img title="LinkedIn" alt="LinkedIn" className="icon" src={IconLinkedIn}></img></a>
+                <a href="https://github.com/cjbates02"><img title="GitHub" alt="GitHub" className="icon" src={IconGitHub}></img></a>
+                <img title="Phone Number" alt="Phone Number" className="icon" src={IconPhone}></img>
+                <img title="Email" alt="Email" className="icon" src={IconEmail}></img>
             </div>
         </div>
     );

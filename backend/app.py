9b69d048de5687b97692d89fdb95556a46113b65
cjbates02 <christianbates02@gmail.com>
2024-11-12 16:13:10 -0500

    created fetch api call to server that contains message from client

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,35 @@
-from flask import Flask, send_file, jsonify
-import os
+from flask import Flask, send_file, jsonify, request
 from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+import os
+import smtplib
+
+load_dotenv()
 
 api = Flask(__name__)
 CORS(api)
 
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Christian",
-        "message": "Hello React!"
-    }
-    return response_body
+FROM_EMAIL = os.getenv('FROM_EMAIL')
+FROM_EMAIL_PASSWORD = os.getenv('FROM_EMAIL_PASSWORD')
+TO_EMAIL = os.getenv('TO_EMAIL')
+SMTP_HOST = os.getev('SMTP_HOST')
+SMTP_PORT = os.getenv('SMTP_PORT')
+
 
 @api.route('/download/resume')
 def send_resume():
-    print('function called')
     file_path = os.path.join('files', 'christianbates.docx')
     if os.path.exists(file_path):
         return send_file(file_path, as_attachment=True)
     else:
         return jsonify({'error': 'file not found'}), 404
 
+
+@api.route('/send-email', methods=['GET', 'POST'])
+def send_email():
+    data = request.get_json()
+    return jsonify({'message': f'Recieved message: {data}'})
+    
+    
 if __name__ == '__main__':
     api.run(debug=True)
--- frontend/src/components/ContactMe.jsx
+++ frontend/src/components/ContactMe.jsx
@@ -1,4 +1,23 @@
+import React, { useState } from 'react';
+
 export default function ContactMe() {
+    const [message, setMessage] = useState('');
+
+    function handleMessageChange(e) {
+        setMessage(e.target.value);
+    }
+
+    function handleMessageSubmit() {
+        fetch('http://127.0.0.1:5000/send-email', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({message}),
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+    }
+
     return (
         <div className="contact-me-container">
             <div className="contact-me-title">
@@ -6,9 +25,9 @@ export default function ContactMe() {
             </div>
             <div className="contact-input-container">
                 <input className="input-email" placeholder="Email"></input>
-                <textarea className="input-message" placeholder="Message"></textarea>
+                <textarea className="input-message" onChange={handleMessageChange} placeholder="Message"></textarea>
                 <div className="submit-container">
-                    <span className="submit-btn">Submit</span>
+                    <span className="submit-btn" onClick={handleMessageSubmit}>Submit</span>
                 </div>
             </div>
         </div>
